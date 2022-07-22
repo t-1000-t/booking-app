429fe45cd71894b11f3acd3593dfe8941803af49
Vladlen <vbranetskiy@rightandabove.com>
2022-07-22 11:58:08 +0300

    dev
    Added: correct package.json, eslint, tsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import logo from 'logo.svg'
+import React from 'react'
+
 import 'App.css'
+import Header from './Header/Header'
+import Main from './Main/Main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        THIS VERSION REACT-18 FOR CORRECTLY Eslint!
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
     </div>
   )
 }
